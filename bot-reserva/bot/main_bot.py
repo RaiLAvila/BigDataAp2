36c1879155b2d154dbbf5067059c9945e3c0f3fd
DESKTOPFB\Fabio <lamperdeavila@gmail.com>
2025-11-15 20:00:40 -0300

    Teste

--- bot-reserva/bot/main_bot.py
+++ bot-reserva/bot/main_bot.py
@@ -2,7 +2,6 @@ from botbuilder.core import ActivityHandler, TurnContext, ConversationState, Use
 from botbuilder.schema import ChannelAccount
 from botbuilder.dialogs import Dialog
 
-# Comentario Teste
 # Dicionário simples para códigos IATA de cidades comuns
 CITY_IATA = {
     "rio de janeiro": "RIO",
